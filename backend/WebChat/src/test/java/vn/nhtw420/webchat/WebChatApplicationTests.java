b0bc938e3a0c5844dbc43ac09ef97784b3f8d2fb
htungitmo420 <nguyentung23112005@gmail.com>
2026-01-23 23:30:26 +0300

    chore: setup project with Java 21 and Gradle

---
+++ backend/WebChat/src/test/java/vn/nhtw420/webchat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nhtw420.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
